c323ae53885e74483ac9ae13980dddadf59971b5
bachtv <vietbach2472004@gmail.com>
2025-12-16 02:10:09 +0000

    deploy nestjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import express from 'express'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+const server = express()
+
+export async function createServer() {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  )
+  app.enableCors()
+  await app.init()
+  return server
 }
-bootstrap();
